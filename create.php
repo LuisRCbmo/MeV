32eee9c1f8ad15f719c7fadfb6dce85b9f8fa193
gary mamani cortez <garymamani12@gmail.com>
2020-07-07 13:08:28 -0400

    encriptacion de la contraseña

--- create.php
+++ create.php
@@ -3,11 +3,12 @@ include("conection.php");
 $user=$_POST['username'];
 $email=$_POST['email'];
 $pass=$_POST['pasw'];
-$repass=$_POST['repasw'];
+$rpass=$_POST['repasw'];
 
-if($user != "" && $email!= "" && $pass!= "" && $repass!= ""){
-    if($pass === $repass){
-        $datos="INSERT INTO student VALUES('$email','$pass','$user')";
+if($user != "" && $email!= "" && $pass!= "" && $rpass!= ""){
+    if($pass === $rpass){
+        $clave=md5('$pass');
+        $datos="INSERT INTO student VALUES('$email','$clave','$user')";
         $conectar=pg_query($db,$datos) or die ("error al introducir datos");
         echo "<head><meta http-equiv='refresh' content='1; url=login.html'></head>";
     }else{
