327912e5d122d5875ee5a1654dc79fd15f2a0dde
gary mamani cortez <garymamani12@gmail.com>
2020-07-07 11:22:55 -0400

    cambiar el BVCHAR por BYTEA

--- Base de datos/MeV.sql
+++ Base de datos/MeV.sql
@@ -92,7 +92,7 @@ DAY
 /*==============================================================*/
 create table STUDENT (
    CORREO               VARCHAR(60)          not null,
-   PASSWORD             VBIN                 not null,
+   PASSWORD             BYTEA                 not null,
    "USER"               VARCHAR(30)          not null,
    constraint PK_STUDENT primary key ("USER", CORREO)
 );
