a5cb507523a3349cbec7ba540ef6b9304813b45f
gary mamani cortez <garymamani12@gmail.com>
2020-07-08 12:02:10 -0400

    nuevo host a probar

--- conection.php
+++ conection.php
@@ -1,10 +1,10 @@
 <?php
 
-  $host="host=localhost";
+  $host="host=bmnuy1syncnwfmr4hz3s-postgresql.services.clever-cloud.com";
   $port="port=5432";
-  $dbname="dbname=MeV";
-  $user="user=postgres";
-  $pass="password=thetripleg18";
+  $dbname="dbname=bmnuy1syncnwfmr4hz3s";
+  $user="user=ulxnwds46l6ztfnqk063";
+  $pass="password=P8IINRf7OA4VwXDL8KC8";
 
   $db= pg_connect("$host $port $dbname $user $pass");
    if(!$db){
