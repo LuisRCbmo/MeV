f266e921dee391605e2a5a6f978b1a8bd48ef949
gary mamani cortez <garymamani12@gmail.com>
2020-07-09 17:20:22 -0400

    completar la salida de datos

--- show.php
+++ show.php
@@ -1,18 +1,16 @@
 <?php
-
 include("conection.php");
+include("login.php");
 
-$smster=$_POST[''];
-
-
+$use_by=$_SESSION["username"];
 $info="SELECT name as clase, full_name as docente,starts as empieza,ends as termina, day as dia,
 cod_cl as aula
 from student_class,class,schedule, time_of
 where student_class.cod_cls=class.cod_cls and student_class.group_of=class.group_of
 and class.cod_cls=schedule.cod_cls and class.group_of=schedule.group_of 
 and student_class.cod_cls=schedule.cod_cls and student_class.group_of=schedule.group_of
-and schedule.cod_ti=time_of.cod_ti AND semester='$smster'";
+and schedule.cod_ti=time_of.cod_ti AND username='$use_by'";
 
-$clases= pg_query($db,);
-
+$clases= pg_query($db,$info);
+$subjects=pg_fetch_array($clases);
 ?>
