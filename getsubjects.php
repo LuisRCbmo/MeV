deff296ee64d30440216028f4843b9b2fa74d268
gary mamani cortez <garymamani12@gmail.com>
2020-07-10 17:20:01 -0400

    valor cambiado al codigo de clase

--- getsubjects.php
+++ getsubjects.php
@@ -3,11 +3,11 @@
 include("conection.php");
 $semest=$_POST['semestre'];
 
-$queryS="SELECT DISTINCT name FROM class WHERE semester='$semest' ORDER BY name ASC";
+$queryS="SELECT DISTINCT cod_cls, name FROM class WHERE semester='$semest' ORDER BY name ASC";
 $resultS=pg_query($db,$queryS) or die("no se pudo conectar consulta");
 $html="";
 while($mat=pg_fetch_array($resultS)){
-    $html= "<option value='".$mat['name']."'>".$mat['name']."</option>";
+    $html= "<option value='".$mat['cod_cls']."'>".$mat['name']."</option>";
     echo $html;
 }
 ?>
